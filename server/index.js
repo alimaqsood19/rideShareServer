fd10b22803787b598d03ece7f8c3788306b447dc
Admin <ali-maqsood@hotmail.com>
2018-03-09 17:27:19 -0500

    CORS middleware

--- server/index.js
+++ server/index.js
@@ -9,6 +9,23 @@ const userRoute = require('./routes/userRoutes');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  //Access for CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,OPTIONS,POST,PUT,DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+  );
+  // Remmovng the cache for most recent comments
+  // res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
 app.use('/user', userRoute);
 
 app.get('/', (req, res) => {
